e50862f75bdc81f3a87a2772caf793df25fa7d9b
于浩 <396819884@qq.com>
2017-06-22 12:07:30 +0800

    detail

--- vueApp/src/components/header/header.vue
+++ vueApp/src/components/header/header.vue
@@ -28,17 +28,40 @@
             <i class="iconfont icon-arrow-right"></i>
         </div>
         <img :src="seller.avatar" class="background">
-        <div v-show="detailShow" class="detail" @click="showDetail">
-            <div class="detail-wrapper clearfix">
-                <div class="detail-content">
-                    <h1 class="name">{{seller.name}}</h1>
-                    <star :size="48" :score="seller.score"></star>
+        <transition name="fade" appear>
+            <div v-show="detailShow" class="detail">
+                <div class="detail-wrapper clearfix">
+                    <div class="detail-content">
+                        <h1 class="name">{{seller.name}}</h1>
+                        <div class="star-wrapper">
+                            <star :size="48" :score="seller.score"></star>
+                        </div>
+                        <div class="title">
+                            <div class="line"></div>
+                            <div class="text">优惠信息</div>
+                            <div class="line"></div>
+                        </div>
+                        <ul v-if="seller.supports" class="supports">
+                            <li class="support-item" v-for="item in seller.supports">
+                                <span class="icon" v-bind:class="supportIcon[item.type]"></span>
+                                <span class="text">{{item.description}}</span>
+                            </li>
+                        </ul>
+                        <div class="title">
+                            <div class="line"></div>
+                            <div class="text">商家公告</div>
+                            <div class="line"></div>
+                        </div>
+                        <div class="detail-bulletin">
+                            <p class="content">{{seller.bulletin}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="detail-close" @click="showDetail">
+                    <i class="iconfont icon-close"></i>
                 </div>
             </div>
-            <div class="detail-close">
-                <i class="iconfont icon-close"></i>
-            </div>
-        </div>
+        </transition>
     </div>
 </template>
 
@@ -180,7 +203,6 @@
                 right: 12px
                 bottom: 9px
                 font-size: 10px
-
         .detail
             position: fixed
             z-index: 100
@@ -189,8 +211,13 @@
             width: 100%
             height: 100%
             overflow-y: auto
-            background: rgba(7, 17, 27.0 .8)
-            fliter: blur(10px)
+            backdrop-filter: blur(10px)
+            background: rgba(7, 17, 27, 0.8)
+            &.fade-enter-active, &.fade-leave-active {
+                transition: opacity 0.5s
+            }
+            &.fade-enter, &.fade-leave-active
+                opacity: 0
             .detail-wrapper
                 width: 100%
                 min-height: 100%
@@ -203,6 +230,61 @@
                         text-align: center
                         font-size: 16px
                         font-weight: 700
+                    .star-wrapper
+                        margin-top: 16px
+                        padding: 2px 0
+                        text-align: center
+                    .title
+                        display: flex
+                        width: 80%
+                        margin: 28px auto 24px
+                        .line
+                            flex: 1
+                            position: relative
+                            top: -6px
+                            border-1px(rgba(255, 255, 255, 0.2))
+                        .text
+                            padding: 0 12px
+                            font-weight: 700
+                            font-size: 14px
+                    .supports
+                        width: 80%
+                        margin: 0 auto
+                        .support-item
+                            padding: 0 12px
+                            margin-bottom: 12px
+                            font-size: 0
+                            &:last-child
+                                margin-bottom: 0
+                            .icon
+                                display: inline-block
+                                width: 16px
+                                height: 16px
+                                margin-right: 6px
+                                background-size: 16px 16px
+                                background-repeat: no-repeat
+                                vertical-align: top
+                                &.decrease
+                                    bg-image('decrease_2')
+                                &.discount
+                                    bg-image('discount_2')
+                                &.guarantee
+                                    bg-image('guarantee_2')
+                                &.invoice
+                                    bg-image('invoice_2')
+                                &.special
+                                    bg-image('special_2')
+                            .text
+                                display: inline-block
+                                line-height: 16px
+                                font-size: 12px
+                    .detail-bulletin
+                        width: 80%
+                        margin: 0 auto
+                        .content
+                            padding: 0 12px
+                            line-height: 24px
+                            font-size: 12px
             .detail-close
                 position: relative
                 width: 32px
